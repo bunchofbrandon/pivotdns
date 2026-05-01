377f64b493f947eefed37091f04997e20522e072
bunchofbrandon <brandonsilva1987@proton.me>
2026-05-02 01:49:54 +0330

    Update route.ts
    more leg

--- app/api/ppxr/route.ts
+++ app/api/ppxr/route.ts
@@ -7,13 +7,19 @@ export async function GET(req: NextRequest) {
     const url = new URL(u);
     const h = new Headers();
     h.set('user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
-    h.set('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8');
+    h.set('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7');
     h.set('accept-language', 'en-US,en;q=0.9');
     h.set('accept-encoding', 'gzip, deflate, br');
-    h.set('sec-fetch-mode', 'navigate');
+    h.set('sec-ch-ua', '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"');
+    h.set('sec-ch-ua-mobile', '?0');
+    h.set('sec-ch-ua-platform', '"Windows"');
     h.set('sec-fetch-site', 'none');
+    h.set('sec-fetch-mode', 'navigate');
+    h.set('sec-fetch-user', '?1');
     h.set('sec-fetch-dest', 'document');
     h.set('upgrade-insecure-requests', '1');
+    h.set('referer', 'https://www.google.com/');
+    h.set('origin', 'https://www.google.com');
     h.set('host', url.host);
 
     const r = await fetch(url.toString(), {
